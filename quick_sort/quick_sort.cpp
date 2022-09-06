02bc0ead90e73b70e12354b02a127eeec06488f9
gofaquan <2660796265@qq.com>
2022-09-06 17:06:17 +0800

    更新快排代码

--- quick_sort/quick_sort.cpp
+++ quick_sort/quick_sort.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-void quick_sort(int q[], int l, int r) {
-    if (l >= r)return; // left >= right 直接退出
+void quick_sort(int q[], int left, int right) {
+    if (left >= right)return; // left >= right 直接退出
 
-    int x = q[l], i = l-1, j = r + 1; // x = queue[left], i = left -1, j = right + 1
+    int x = q[left], i = left - 1, j = right + 1;
+
     while (i < j) { // i < j 时
         do i++; while (q[i] < x); // 左指针右移，直到当前指向数不再小于x
         do j--; while (q[j] > x); // 右指针左移，直到当前指向数不再大于x
         if (i < j) swap(q[i], q[j]); // 如果此时满足 i<j ，就 swap 交换数组对应下标值
     }
-    quick_sort(q, l, j); // 递归调用，新的 right 为 j
-    quick_sort(q,j+1,r); // 递归调用，新的 left 为 j+1
+    quick_sort(q, left, j); // 递归调用，新的 right 为 j
+    quick_sort(q, j + 1, right); // 递归调用，新的 left 为 j+1
 }
 
 
@@ -24,10 +25,10 @@ int main() {
         scanf("%d", &q[i]);
     }
 
-    quick_sort(q,0, n-1); // left 第一个元素，right 最后一个元素
+    quick_sort(q, 0, n - 1); // left 第一个元素，right 最后一个元素
 
     for (int i = 0; i < n; ++i) {
-        printf("%d ",q[i]);
+        printf("%d ", q[i]);
     }
     return 0;
 }
--- quick_sort/quick_sort.go
+++ quick_sort/quick_sort.go
@@ -2,28 +2,25 @@ package main
 
 import "fmt"
 
+
 func quickSort(queue []int, left, right int) {
 	if left >= right {
 		return
 	}
 
-	x, i, j := queue[left], left, right
+	x, i, j := queue[left], left-1, right+1
 	for i < j {
-		for queue[i] < x {
-			i++
+		for i++; queue[i] < x; i++ {
+		}
+		for j--; queue[j] > x; j-- {
 		}
 
-		for queue[j] > x {
-			j--
-		}
 		if i < j {
 			queue[i], queue[j] = queue[j], queue[i]
 		}
 	}
-
 	quickSort(queue, left, j)
 	quickSort(queue, j+1, right)
-
 }
 
 func main() {
