45fd435b4af333498eb5e7bd5f2ce29d2271da86
gofaquan <2660796265@qq.com>
2022-10-28 16:38:26 +0800

    56

---
+++ leecode/56_test.go
@@ -0,0 +1,73 @@
+package leecode
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testArray = [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+
+func TestQuickSort(t *testing.T) {
+	quickSort(testArray, 0, len(testArray)-1)
+	fmt.Println(testArray)
+}
+
+func TestMerge(t *testing.T) {
+	fmt.Println(merge(testArray))
+}
+
+func merge(intervals [][]int) [][]int {
+	var res [][]int
+	quickSort(intervals, 0, len(intervals)-1)
+	// 0 <= starti <= endi <= 10^4
+	st, end := -1, -1
+
+	for _, interval := range intervals {
+		if end < interval[0] {
+			if st != -1 {
+				res = append(res, []int{st, end})
+			}
+			st, end = interval[0], interval[1]
+		} else {
+			end = max(interval[1], end)
+		}
+	}
+
+	if st != -1 {
+		res = append(res, []int{st, end})
+	}
+
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+var tmp [100000]int
+
+func quickSort(queue [][]int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	leftBoard, rightBoard, Val := left-1, right+1, queue[left][0]
+
+	for leftBoard < rightBoard {
+		for leftBoard++; queue[leftBoard][0] < Val; leftBoard++ {
+		}
+		for rightBoard--; queue[rightBoard][0] > Val; rightBoard-- {
+		}
+
+		if leftBoard < rightBoard {
+			queue[leftBoard], queue[rightBoard] = queue[rightBoard], queue[leftBoard]
+		}
+	}
+
+	quickSort(queue, left, rightBoard)
+	quickSort(queue, rightBoard+1, right)
+}
