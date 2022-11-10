616ac0a678450640dae2aeba1e76383a4dd454d1
gofaquan <2660796265@qq.com>
2022-11-10 22:18:19 +0800

    27

---
+++ leecode/27_test.go
@@ -0,0 +1,26 @@
+package leecode
+
+func removeElement1(nums []int, val int) int {
+	left := 0
+	for _, v := range nums { // v 即 nums[right]
+		if v != val {
+			nums[left] = v
+			left++
+		}
+	}
+	return left
+}
+
+// 快慢指针
+func removeElement(nums []int, val int) int {
+	length := len(nums)
+	res := 0
+	for i := 0; i < length; i++ {
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+	nums = nums[:res]
+	return res
+}
---
+++ leecode/695_test(x).go
@@ -0,0 +1,21 @@
+package leecode
+
+//var p [10000]int
+//var size [10000]int
+//
+//func maxAreaOfIsland(grid [][]int) int {
+//	var result int
+//
+//	return result
+//}
+//
+//func connect(p []int, x, y int) {
+//
+//}
+//
+//func find(x int) int {
+//	if p[x] != x {
+//		p[x] = find(p[x])
+//	}
+//	return p[x]
+//}
