e13535c54f2e9af4fa0cf54c3c548918a3141cd0
gofaquan <2660796265@qq.com>
2022-11-20 21:19:45 +0800

    743 floyd

--- leecode/743-2_test.go
+++ leecode/743-2_test.go
@@ -2,6 +2,44 @@ package leecode
 
 import "math"
 
+func networkDelayTime(times [][]int, n, k int) (ans int) {
+	const inf = math.MaxInt64 / 2
+	g := make([][]int, n)
+	for i := 0; i < n; i++ {
+		g[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			if i == j {
+				g[i][j] = 0
+			} else {
+				g[i][j] = inf
+			}
+		}
+	}
+
+	for _, time := range times {
+		g[time[0]-1][time[1]-1] = time[2]
+	}
+
+	for p := 0; p < n; p++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				g[i][j] = min(g[i][j], g[i][p]+g[p][j])
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		ans = max(g[k-1][i], ans)
+	}
+
+	if ans >= inf {
+		return -1
+	} else {
+		return ans
+	}
+	return
+}
+
 type Edge struct {
 	a, b, c int
 }
