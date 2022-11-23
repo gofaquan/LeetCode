d47a7ed4dacb9bb4d64f2c376d7c7c6cd6cdf7c5
gofaquan <2660796265@qq.com>
2022-11-23 15:51:28 +0800

    204 埃式筛法, 线性筛法

--- leecode/204_test.go
+++ leecode/204_test.go
@@ -35,3 +35,54 @@ func countPrimes1(n int) (cnt int) {
 func TestC(t *testing.T) {
 	countPrimes1(10)
 }
+
+// 埃式筛法
+func countPrimes2(n int) (cnt int) {
+	st := make([]bool, n+1)
+	primes := make([]int, n+1)
+
+	for i := 2; i < n; i++ {
+		if !st[i] {
+			primes[cnt] = n
+			cnt++
+
+			for j := 2 * i; j < n; j += i {
+				st[j] = true
+			}
+		}
+
+	}
+
+	return
+}
+
+func TestC2(t *testing.T) {
+	println(countPrimes2(2))
+}
+
+// 线性筛法
+func countPrimes(n int) (cnt int) {
+	st := make([]bool, n+1)
+	primes := make([]int, n+1)
+
+	for i := 2; i < n; i++ {
+		if !st[i] {
+			primes[cnt] = i
+			cnt++
+		}
+
+		for j := 0; primes[j] <= n/i; j++ {
+			st[primes[j]*i] = true
+			if i%primes[j] == 0 {
+				break
+			}
+		}
+
+	}
+
+	return
+}
+
+func TestC12(t *testing.T) {
+	println(countPrimes(10))
+}
