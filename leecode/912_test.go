e1f59c2d8338f3a22e2a8aef1e261107f71772e6
gofaquan <2660796265@qq.com>
2022-12-04 10:55:48 +0800

    56 merge sort

--- leecode/912_test.go
+++ leecode/912_test.go
@@ -5,12 +5,50 @@ import (
 	"testing"
 )
 
-func sortArray(nums []int) []int {
-	quickSort(nums, 0, len(nums)-1)
+func _912sortArray(nums []int) []int {
+	//_912QuickSort(nums, 0, len(nums)-1)
+	var tmpArr = make([]int, len(nums))
+
+	var MergeSort func(nums []int, l, r int)
+	MergeSort = func(nums []int, l, r int) {
+		if l >= r {
+			return
+		}
+		mid := (l + r) / 2
+		MergeSort(nums, l, mid)
+		MergeSort(nums, mid+1, r)
+
+		i, j, k := l, mid+1, 0
+		for i <= mid && j <= r {
+			if nums[i] > nums[j] {
+				tmpArr[k] = nums[j]
+				k, j = k+1, j+1
+			} else {
+				tmpArr[k] = nums[i]
+				k, i = k+1, i+1
+			}
+		}
+
+		for i <= mid {
+			tmpArr[k] = nums[i]
+			k, i = k+1, i+1
+		}
+		for j <= r {
+			tmpArr[k] = nums[j]
+			k, j = k+1, j+1
+		}
+
+		for i, j = l, 0; i <= r; i, j = i+1, j+1 {
+			nums[i] = tmpArr[j]
+		}
+
+	}
+
+	MergeSort(nums, 0, len(nums)-1)
 	return nums
 }
 
-func quickSort(nums []int, l, r int) {
+func _912QuickSort(nums []int, l, r int) {
 	if l >= r {
 		return
 	}
@@ -27,11 +65,12 @@ func quickSort(nums []int, l, r int) {
 		}
 	}
 
-	quickSort(nums, l, j)
-	quickSort(nums, j+1, r)
-
+	_912QuickSort(nums, l, j)
+	_912QuickSort(nums, j+1, r)
 }
 
-func TestSortArray(t *testing.T) {
-	fmt.Println(sortArray([]int{5, 2, 3, 1}))
+var a = []int{5, 2, 3, 1}
+
+func TestQuickSort1(t *testing.T) {
+	fmt.Println(_912sortArray(a))
 }
