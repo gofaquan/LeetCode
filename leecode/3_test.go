a3de0b420d17510ab5b26fb96084707a84c08cdc
gofaquan <2660796265@qq.com>
2022-12-05 15:09:00 +0800

    3 map & slice

--- leecode/3_test.go
+++ leecode/3_test.go
@@ -1,6 +1,6 @@
 package leecode
 
-func lengthOfLongestSubstring(s string) int {
+func _3lengthOfLongestSubstring(s string) int {
 	var a = make([]int, len(s))
 	res := 0
 
@@ -16,4 +16,21 @@ func lengthOfLongestSubstring(s string) int {
 		}
 	}
 	return res
+}
+
+func _3lengthOfLongestSubstring2(s string) (res int) {
+	m := map[uint8]int{}
+	for i, j := 0, 0; i < len(s); i++ {
+		m[s[i]]++
+		for m[s[i]] > 1 {
+			m[s[j]]--
+			j++
+		}
+
+		if res < i-j+1 {
+			res = i - j + 1
+		}
+	}
+
+	return
 }
