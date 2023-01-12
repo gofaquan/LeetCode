5c72f1487765a063cb877ecebff39cd8cca94850
gofaquan <2660796265@qq.com>
2023-01-12 11:44:06 +0800

    🗡☞offer day26

--- 1.go
+++ 1.go
@@ -1,14 +1,20 @@
 package main
 
-type Node struct {
-	Val   int
-	Left  *Node
-	Right *Node
-}
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+//	type Node struct {
+//		Val   int
+//		Left  *Node
+//		Right *Node
+//	}
 type ListNode struct {
-	Val   int
-	Left  *ListNode
-	Right *ListNode
+	Val  int
+	Next *ListNode
 }
 type TreeNode struct {
 	Val   int
@@ -23,6 +29,45 @@ func max(x, y int) int {
 	return y
 }
 
-func main() {
+func strToInt(s string) int {
+	str := strings.TrimSpace(s)
+	if len(str) == 0 {
+		return 0
+	}
+	i := 0
 
+	if !(str[i] == '-' || str[i] == '+' || str[i] >= '0' && str[i] <= '9') {
+		return 0
+	}
+	i++
+
+	for ; i < len(str); i++ {
+		if !(str[i] >= '0' && str[i] <= '9') {
+			break
+		}
+	}
+	maxStr := strconv.Itoa(math.MaxInt32)
+	var newStr string
+	if str[0] == '-' {
+		newStr = str[1:i]
+		if newStr > maxStr && len(newStr) >= len(maxStr) {
+			return math.MinInt32
+		} else {
+			res, _ := strconv.Atoi(newStr)
+			return -res
+		}
+	} else {
+		newStr = str[:i]
+		if newStr > maxStr || len(newStr) >= len(maxStr) {
+			return math.MaxInt32
+		} else {
+			res, _ := strconv.Atoi(newStr)
+			return res
+		}
+	}
+
+}
+
+func main() {
+	fmt.Println(strToInt(" "))
 }
